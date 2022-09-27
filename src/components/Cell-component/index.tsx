e856ddb1a80ea3e38d903b26db435c394031386c
olesiklesha <olesik736@gmail.com>
2022-09-27 13:52:16 +0300

    feat: implement cell highlighting and base figure move

--- src/components/Cell-component/index.tsx
+++ src/components/Cell-component/index.tsx
@@ -3,11 +3,21 @@ import {Cell} from "../../models/Cell";
 
 interface CellProps {
   cell: Cell
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
   return (
-    <div className={['cell', cell.color].join(' ')}>
+    <div
+      className={[
+        'cell',
+        cell.color, selected ? 'selected' : '',
+        cell.available && cell.figure ? 'attacked' : ''
+      ].join(' ')}
+      onClick={() => click(cell)}
+    >
+      {cell.available && !cell.figure && <div className='available'/>}
       {cell.figure?.logo && <img src={cell.figure.logo} alt={cell.figure.name}/>}
     </div>
   );
